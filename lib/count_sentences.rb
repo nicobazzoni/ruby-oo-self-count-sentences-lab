9d7c14595e2076a4d9ad85165079fbd10080401c
Nico Bazzoni <you@example.com>
2020-11-28 17:58:05 -0500

    commit tree 4334df8d41b42304a865a7d38e27c7bc4132c0bf

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #binding.pry 
+    if self.end_with?(".")
+    return true
+    else
+    return false
+    end
+
   end
 
   def question?
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
-
-  end
-
-  def count_sentences
-
+if self.end_with?("!")
+  return true
+else
+  return false
   end
 end
+
+  def count_sentences
+self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+  end
+end 
